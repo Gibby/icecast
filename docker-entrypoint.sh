2ba241253c34080537242ecfa7543b3b8bed8800
Chris Gibson <git@twoitguys.com>
2019-02-13 22:53:20 -0500

    Fixes for better use of variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,35 +1,16 @@
 #!/bin/sh
 
-if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
-    sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_RELAY_PASSWORD" ]; then
-    sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_ADMIN_PASSWORD" ]; then
-    sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_ADMIN_USERNAME" ]; then
-    sed -i "s/<admin-user>[^<]*<\/admin-user>/<admin-user>$ICECAST_ADMIN_USERNAME<\/admin-user>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_ADMIN_EMAIL" ]; then
-    sed -i "s/<admin>[^<]*<\/admin>/<admin>$ICECAST_ADMIN_EMAIL<\/admin>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_LOCATION" ]; then
-    sed -i "s/<location>[^<]*<\/location>/<location>$ICECAST_LOCATION<\/location>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_HOSTNAME" ]; then
-    sed -i "s/<hostname>[^<]*<\/hostname>/<hostname>$ICECAST_HOSTNAME<\/hostname>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_PORT" ]; then
-    sed -i "s/<port>[^<]*<\/port>/<port>$ICECAST_PORT<\/port>/g" /etc/icecast.xml
-fi
-if [ -n "$ICECAST_MAX_CLIENTS" ]; then
-    sed -i "s/<clients>[^<]*<\/clients>/<clients>$ICECAST_MAX_CLIENTS<\/clients>/g" /etc/icecast.xml
-fi
+ICECAST_ADMIN_USERNAME=${ICECAST_ADMIN_USERNAME:-admin}
+ICECAST_ADMIN_EMAIL=${ICECAST_ADMIN_EMAIL:-none}
+ICECAST_ADMIN_PASSWORD=${ICECAST_ADMIN_PASSWORD:-admin}
+ICECAST_SOURCE_PASSWORD=${ICECAST_SOURCE_PASSWORD:-admin}
+ICECAST_RELAY_PASSWORD=${ICECAST_RELAY_PASSWORD:-admin}
+ICECAST_LOCATION=${ICECAST_LOCATION:-MilkyWay}
+ICECAST_HOSTNAME=${ICECAST_HOSTNAME:-Pluto}
+ICECAST_PORT=${ICECAST_PORT:-8000}
+ICECAST_MAX_CLIENTS=${ICECAST_MAX_CLIENTS:-100}
+ICECAST_MAX_SOURCES=${ICECAST_MAX_SOURCES:-100}
 
-if [ -n "$ICECAST_MAX_SOURCES" ]; then
-    sed -i "s/<sources>[^<]*<\/sources>/<sources>$ICECAST_MAX_SOURCES<\/sources>/g" /etc/icecast.xml
-fi
+envsubst < /icecast.tmpl > /etc/icecast.xml
 
-exec "$@"
+icecast -c /etc/icecast.xml
